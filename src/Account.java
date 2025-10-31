e46caa05fe5eecc2c3afde7da58301b99c64e6c3
HeyJudo <jude.sangalang09@gmail.com>
2025-10-31 13:26:29 +0800

    New Filex

---
+++ src/Account.java
@@ -0,0 +1,44 @@
+abstract public class Account {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public Account(String accountNumber, String accountHolderName, double Balance){
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = Balance;
+    }
+    public void deposit (double amount){
+        this.balance+= amount;
+        System.out.println("Amount Deposited: "+ amount);
+        System.out.println("New Balance: " + this.balance);
+    }
+    public boolean withdraw (double amount){
+        if(amount > this.balance){
+            System.out.println("Insufficient Balance!");
+            return false;
+        } else {
+            this.balance-=amount;
+            System.out.println("Withdrawn: " + amount);
+            System.out.println("Balance: " + this.balance);
+            return true;
+        }
+    }
+    public double getBalance (){
+        return this.balance;
+    }
+    public String getAccountNumber(){
+        return this.accountNumber;
+    }
+    public String getAccountHolderName(){
+        return this.accountHolderName;
+    }
+    public void displayAccountDetails(){
+        System.out.println("===========================");
+        System.out.println("Account Name: "+ this.accountHolderName);
+        System.out.println("Account Number: "+ this.accountNumber);
+        System.out.println("Account Balance: "+ this.balance);
+        System.out.println("===========================");
+    }
+}
+
