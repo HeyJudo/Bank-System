166a1aa8bead505f48bcf09e7942f3077b51145d
HeyJudo <jude.sangalang09@gmail.com>
2025-11-01 14:17:03 +0800

    New Filex

---
+++ src/BusinessAccount.java
@@ -0,0 +1,35 @@
+public class BusinessAccount extends Account {
+    private double transactionLimit;
+    private double transactionFeePercentage;
+
+    public BusinessAccount(String accountNumber, String accountHolderName, double Balance, double transactionLimit, double transactionFeePercentage) {
+        super(accountNumber, accountHolderName, Balance);
+        this.transactionLimit = transactionLimit;
+        this.transactionFeePercentage = transactionFeePercentage;
+    }
+    public double getTransactionLimit() {
+        return transactionLimit;
+    }
+    public double getTransactionFeePercentage() {
+        return transactionFeePercentage;
+    }
+
+    public void  applyTransactionFee (double transactionAmount){
+        double fee  = transactionAmount*this.transactionFeePercentage;
+        super.withdraw(fee);
+        System.out.println("Transaction Fee is now $" + fee);
+        System.out.println("New Balance: " + this.getBalance());
+    }
+
+    @Override
+    public boolean withdraw (double amount) {
+        if(amount > this.transactionLimit){
+            System.out.println("Amount Exceeded!");
+            return false;
+        } else {
+            return super.withdraw(amount);
+        }
+    }
+
+
+}
