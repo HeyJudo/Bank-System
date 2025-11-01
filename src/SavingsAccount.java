40fa7e0252c2cee312218fae8d3d25703f36350f
HeyJudo <jude.sangalang09@gmail.com>
2025-11-01 11:13:48 +0800

    New Filex

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,2 +1,17 @@
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, String accountHolderName, double balance, double interestRate) {
+        super (accountNumber, accountHolderName, balance);
+        this.interestRate = interestRate;
+    }
+    public void applyInterest(){
+        double interest = this.getBalance()*this.interestRate;
+        System.out.printf("Interest Earned: %.2f", interest);
+        this.deposit(interest);
+        System.out.println("New Balance: "+ this.getBalance());
+    }
+    public double getInterestRate() {
+        return this.interestRate;
+    }
 }
