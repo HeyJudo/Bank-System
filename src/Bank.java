4e7b0457526e2eec3eaa23548dd1de0ec86ec373
HeyJudo <jude.sangalang09@gmail.com>
2025-11-08 00:04:18 +0800

    New Filex

--- src/Bank.java
+++ src/Bank.java
@@ -10,6 +10,23 @@ public class Bank {
     public void addCustomer(Customer customer){
         customers.add(customer);
         System.out.println("Successfully added "+ customer.getCustomerName());
+    }
+    public void removeCustomer(String customerId){
+        if(customers.isEmpty()){
+            System.out.println("There are no customer registered!");
+            return;
+        }
+        for (Customer c: customers){
+            if(c.getCustomerId().equals(customerId)){
+                customers.remove(c);
+                System.out.println("Successfully removed "+ c.getCustomerName());
+                return;
+            }
+        }
+        System.out.println("Customer not found!");
+        return;
+    }
+    public Customer getCustomer(String costumerId){
 
     }
 
